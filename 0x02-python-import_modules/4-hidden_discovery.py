bbfa4f55c8c01e7129f7af9cf72e9f99ecb3a027
root <root@DESKTOP-TRN89PE.localdomain>
2026-08-03 12:06:58 +0100

    program that prints all the names defined by the compiled module hidden_4.pyc

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    import hidden_4
+
+    name = dir(hidden_4)
+
+    for name in sorted(names):
+        if not name.startswith("__"):
+            print(name)
