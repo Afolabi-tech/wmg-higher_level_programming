eb1ea9ab31e053fb92f08d150f78d8ffa37ce123
root <root@DESKTOP-TRN89PE.localdomain>
2026-09-09 13:41:10 +0100

    class MyList that inherits from list and prints the list, but sorted (ascending sort)

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+""" Defines a class MyList that inherits from list.
+
+"""
+
+class MyList(list):
+    """ A list with a method that prints the list sorted . """
+
+    def print_sorted(self):
+        """ Prints the list in ascending sorted order. """
+
+        print(sorted(self))
