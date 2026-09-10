e0dd858296af68076969aeb2ed28f4ae767e0c55
root <root@DESKTOP-TRN89PE.localdomain>
2026-09-10 18:01:34 +0100

    class BaseGeometry - def area(self) that raises an Exception message

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" 
+6-base_geometry.py
+
+A class BaseGeometry
+"""
+
+class BaseGeometry:
+    """Public instance method that raises an Exception with the message area() is not implemented """
+    def area(self):
+        raise Exception("area() is not implemented")
