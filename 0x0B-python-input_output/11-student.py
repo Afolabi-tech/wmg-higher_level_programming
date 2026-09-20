e45fcca183b5b2f738f03a34784e1428ef567e39
AfobStudio <afolabiolutobis.aos@gmail.com>
2026-09-20 13:58:13 +0100

    to_json (serialization) and reload_from_json (deserialization) let you convert an instance to a dictionary and rebuild its state from one.

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Module that defines a Student class."""
+
+
+class Student:
+    """Defines a student by first name, last name and age."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance.
+
+        If attrs is a list of strings, only attribute names contained
+        in this list are retrieved. Otherwise, all attributes are retrieved.
+        """
+        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__.copy()
+
+    def reload_from_json(self, json):
+        
+        """Replaces all attributes of the Student instance.
+
+        Args:
+            json: a dictionary where each key is a public attribute name
+                  and each value is the value of that attribute.
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
